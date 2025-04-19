5f8c7fa33a41667539218e4f61b8098d1bbc6a09
b14nc444 <b14nc4@naver.com>
2025-04-20 00:34:54 +0900

    style: update OptionBox icon to arrow_forward_ios

--- lib/screens/location_permission_screen.dart
+++ lib/screens/location_permission_screen.dart
@@ -20,7 +20,7 @@ class LocationPermissionScreen extends StatelessWidget {
                 children: [AppAssets.logoImage],
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 32),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
@@ -29,12 +29,22 @@ class LocationPermissionScreen extends StatelessWidget {
                     '내 위치 기반 날씨 정보로 옷차림을 추천해드릴게요.',
                     style: TextStyle(fontSize: 16, color: AppColors.gray900),
                   ),
-                  const SizedBox(height: 40),
+                  const SizedBox(height: 32),
                   Column(
                     children: [
-                      const OptionBox('위치 권한 허용하기'),
+                      OptionBox(
+                        '위치 권한 허용하기',
+                        onTap: () {
+                          print('Tapped!');
+                        },
+                      ),
                       const SizedBox(height: 8),
-                      const OptionBox('수동으로 위치 설정하기'),
+                      OptionBox(
+                        '수동으로 위치 설정하기',
+                        onTap: () {
+                          print('Tapped');
+                        },
+                      ),
                     ],
                   ),
                 ],
@@ -49,32 +59,37 @@ class LocationPermissionScreen extends StatelessWidget {
 
 class OptionBox extends StatelessWidget {
   final String label;
+  final VoidCallback? onTap;
 
-  const OptionBox(this.label, {super.key});
+  const OptionBox(this.label, {this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            label,
-            style: const TextStyle(color: AppColors.gray700, fontSize: 14),
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-          const SizedBox(
-            width: 16,
-            height: 16,
-            child: DecoratedBox(
-              decoration: BoxDecoration(color: AppColors.gray300),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(color: AppColors.gray700, fontSize: 14),
             ),
-          ),
-        ],
+            const Icon(
+              Icons.arrow_forward_ios,
+              color: AppColors.gray500,
+              size: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
